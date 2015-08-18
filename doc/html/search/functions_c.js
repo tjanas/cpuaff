7b31ab09e4acf96cb3e740df213c620d241e9e37
Daniel C. Dillon <dcdillon@gmail.com>
2015-08-18 12:20:35 -0500

    Updated documentation and pushed actual 1.0.0 release tarball.

---
+++ doc/html/search/functions_c.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['set_5faffinity',['set_affinity',['../classcpuaff_1_1impl_1_1basic__affinity__manager.html#a2dd11da4607d289948d57596d15fd843',1,'cpuaff::impl::basic_affinity_manager::set_affinity()'],['../classcpuaff_1_1impl_1_1basic__affinity__stack.html#a2d98739ec014dc12402d409e794245df',1,'cpuaff::impl::basic_affinity_stack::set_affinity()']]],
+  ['size',['size',['../classcpuaff_1_1impl_1_1basic__round__robin__allocator.html#a1a058fda214a9680eeedb864c8a45809',1,'cpuaff::impl::basic_round_robin_allocator']]],
+  ['socket',['socket',['../classcpuaff_1_1cpu__spec.html#a4b7dcbbc5d75838d4417100d9ba6a830',1,'cpuaff::cpu_spec::socket() const '],['../classcpuaff_1_1cpu__spec.html#a12a493071577deed53edd9457582ee98',1,'cpuaff::cpu_spec::socket(const socket_type &amp;socket)'],['../classcpuaff_1_1impl_1_1basic__cpu.html#ab899025767185ba7adc03390d18a2a75',1,'cpuaff::impl::basic_cpu::socket()']]],
+  ['spec',['spec',['../classcpuaff_1_1impl_1_1basic__cpu.html#a32c7ab9873bccb72fd224ca718cfc5a4',1,'cpuaff::impl::basic_cpu::spec()'],['../classcpuaff_1_1impl_1_1basic__pci__device.html#aff8e23b4d6887a4f5670f2e336cb57d0',1,'cpuaff::impl::basic_pci_device::spec()']]]
+];
---
+++ doc/html/search/related_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['operator_3c_3c',['operator&lt;&lt;',['../classcpuaff_1_1cpu__spec.html#a79b54a6358ce161255563dd08cc32b54',1,'cpuaff::cpu_spec']]],
+  ['operator_3e_3e',['operator&gt;&gt;',['../classcpuaff_1_1cpu__spec.html#ab1e65d633d40b3d5014afab9edfbde82',1,'cpuaff::cpu_spec']]]
+];
